9369b848c4bd7518d242d019d48e731170246179
MrZloHex <zlo.alex.it@gmail.com>
2021-11-18 15:51:09 +0200

    PARSE

--- ocpp/include/OCPP.h
+++ ocpp/include/OCPP.h
@@ -2,9 +2,20 @@
 
 #define OCPP_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
 #ifndef OCPP
 #define OCPP
 #endif
+
+
+#define MESSAGE_TYPE_ID 0
+#define MESSAGE_ID      1
+#define ACTION          2
+#define PAYLOAD         3
 
 #define CALL       2
 #define CALLRESULT 3
@@ -18,12 +29,30 @@ typedef struct
 } OCPPConfig;
 
 
+typedef struct
+{
+    const char messageID[36];
+
+    const char *action;
+
+    const char *payload; // JSON
+
+} OCPPCall;
+
+
 typedef unsigned char result;
 
 OCPP result
 handle_call
 (
     OCPPConfig *_cfg,
+    const char *_call
+);
+
+
+OCPP OCPPCall
+parse_call
+(
     const char *_call
 );
 
--- ocpp/src/OCPP.c
+++ ocpp/src/OCPP.c
@@ -7,5 +7,63 @@ handle_call
     const char *_call
 )
 {
+    printf("NEW CALL REQUEST: `%s`\n", _call);
+    OCPPCall call = parse_call(_call);
+
     return 0;
+}
+
+
+OCPPCall
+parse_call
+(
+    const char *_call
+)
+{
+    OCPPCall call;
+
+    uint8_t field = MESSAGE_TYPE_ID;
+
+    uint8_t message_type_id;
+    char    message_id[36];
+    char    action[100];
+    char    *json = (char*) malloc(sizeof(char)*1024);
+
+    char    *buffer = (char *) malloc(sizeof(char)*1024);
+    size_t  index_buf = 0;
+
+
+    uint8_t data_in = 0;
+
+
+    for (size_t i = 0; i < strlen(_call); ++i)
+    {
+        char ch = _call[i];
+        switch (ch)
+        {
+            case '[':
+                data_in++;
+                break;
+            case ']':
+                data_in--;
+                break;
+            default:
+                buffer[index_buf++] = ch;
+                break;
+        }
+        if (data_in < 0)
+        {
+            fprintf(stderr, "INVALID REQUEST\n");
+            exit(1);
+        }
+        else if (data_in == 1)
+        {
+            if (field == MESSAGE_TYPE_ID)
+            {
+                
+            }
+        }
+    }
+
+    return call;
 }
