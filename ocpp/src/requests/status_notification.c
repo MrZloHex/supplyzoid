9e311328e3508833c72af3cde7a5b136f8c1d763
MrZloHex <zlo.alex.it@gmail.com>
2021-12-05 18:49:34 +0200

    STATUS

---
+++ ocpp/src/requests/status_notification.c
@@ -0,0 +1,19 @@
+#include "requests/status_notification.h"
+
+void
+ocpp_status_notification_req
+(
+    OCPP *ocpp
+)
+{
+
+}
+
+void
+ocpp_status_notification_conf
+(
+    OCPP *ocpp
+)
+{
+    
+}
