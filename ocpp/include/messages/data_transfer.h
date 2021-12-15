f977037e26903607037bd74ac80557bc34e0547f
MrZloHex <zlo.alex.it@gmail.com>
2021-12-15 22:39:51 +0200

    DATA TRANSFER

---
+++ ocpp/include/messages/data_transfer.h
@@ -0,0 +1,18 @@
+#ifndef DATA_TRANSFER_H_
+#define DATA_TRANSFER_H_
+
+#include "OCPP.h"
+
+void
+ocpp_data_transfer_req
+(
+	OCPP *ocpp
+);
+
+void
+ocpp_data_transfer_conf
+(
+	OCPP *ocpp
+);
+
+#endif /* DATA_TRANSFER_H_ */
---
+++ ocpp/src/messages/data_transfer.c
@@ -0,0 +1,20 @@
+#include "messages/data_transfer.h"
+
+
+void
+ocpp_data_transfer_req
+(
+	OCPP *ocpp
+)
+{
+
+}
+
+void
+ocpp_data_transfer_conf
+(
+	OCPP *ocpp
+)
+{
+
+}
