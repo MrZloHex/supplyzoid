248872de71c6087e3f99440d5182cfdbb8848e43
MrZloHex <zlo.alex.it@gmail.com>
2022-02-26 15:59:13 +0200

    RAPI TESTS

--- armm0plus/test/rapi_tests.c
+++ armm0plus/test/rapi_tests.c
@@ -8,7 +8,7 @@ void
 rapi_analyze_test(void)
 {
     RAPI rapi = {
-        .buf_index = 0,
+        .buf_index = 16,
         .token_index = 0,
         .buf_cmd = "$AB 00 D7.1.6^53"
     };
@@ -18,5 +18,11 @@ rapi_analyze_test(void)
 void
 rapi_chksum_test(void)
 {
-    TEST_ASSERT(1);
+    RAPI rapi = {
+        .buf_index = 14,
+        .token_index = 0,
+        .buf_cmd = "$AB 00 D7.1.6^"
+    };
+    rapi_app_chksum(&rapi);
+    TEST_ASSERT_EQUAL_STRING("$AB 00 D7.1.6^53\r", rapi.buf_cmd);
 }
